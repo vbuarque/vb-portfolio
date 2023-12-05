b6727002af6ada3820f00fda317068ecd5256a40
vbuarque <viniciusbuarque17@gmail.com>
2023-12-05 15:25:21 -0300

    feat: criando o `ThemeProvider.jsx` para implementar os temas da aplicação

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import './globals.css'
 //Components
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+// theme provider
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +16,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ThemeProvider attribute='class' defaultTheme='light'>
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   )
