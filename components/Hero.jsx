dfdd9b90e7ae8e66a192690ae91b1223e831b5f3
vbuarque <viniciusbuarque17@gmail.com>
2023-12-07 01:53:19 -0300

    feat: finalizando hero section e badges

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -41,12 +41,42 @@ const Hero = () => {
               </Button>
             </div>
             {/* socials */}
-            <Socials />
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
           </div>
           {/* image */}
-          <div className="flex justify-center items-center"></div>
-          {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            {/* badge 1 */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText="Years Of Experience"
+            />
+             {/* badge 2 */}
+             <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText='k'
+              badgeText="Finished Projects"
+            />
+              {/* badge 3 */}
+              <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText='k'
+              badgeText="Happy Clients"
+            />
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/meAvatar.png"
+            />
+          </div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
